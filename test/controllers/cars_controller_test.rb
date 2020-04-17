70389be3656ff0f18bfa2d895ed53189f8514849
Scott Fleming <sdflem@gmail.com>
2020-04-17 08:38:00 -0500

    Add functional tests for cars controller actions

--- test/controllers/cars_controller_test.rb
+++ test/controllers/cars_controller_test.rb
@@ -2,49 +2,64 @@ require 'test_helper'
 
 class CarsControllerTest < ActionDispatch::IntegrationTest
 
-  # setup do
-  #   @car = cars(:one)
-  # end
+  setup do
+    @car = cars(:one)
+  end
 
-  # test "should get index" do
-  #   get cars_url
-  #   assert_response :success
-  # end
+  test "should get index" do
+    get cars_url
+    assert_response :success
+    assert_match "Cars", @response.body
+    assert_match @car.make, @response.body
+    assert_match @car.model, @response.body
+    assert_match @car.year.to_s, @response.body
+    assert_equal "index", @controller.action_name
+  end
 
-  # test "should get new" do
-  #   get new_car_url
-  #   assert_response :success
-  # end
+  test "should get new" do
+    get new_car_url
+    assert_response :success
+  end
 
-  # test "should create car" do
-  #   assert_difference('Car.count') do
-  #     post cars_url, params: { car: { make: @car.make, model: @car.model, year: @car.year } }
-  #   end
+  test "should create car" do
+    assert_difference('Car.count') do
+      post cars_url, params: { car: { make: @car.make, model: @car.model, year: @car.year } }
+    end
+    assert_redirected_to car_url(Car.last)
+  end
 
-  #   assert_redirected_to car_url(Car.last)
-  # end
+  test "should not create car with missing attribute values" do
+    assert_no_difference('Car.count') do
+      post cars_url, params: { car: { make: nil, model: nil, year: nil } }
+    end
+    assert_response :success
+  end
 
-  # test "should show car" do
-  #   get car_url(@car)
-  #   assert_response :success
-  # end
+  test "should show car" do
+    get car_url(@car)
+    assert_response :success
+  end
 
-  # test "should get edit" do
-  #   get edit_car_url(@car)
-  #   assert_response :success
-  # end
+  test "should get edit" do
+    get edit_car_url(@car)
+    assert_response :success
+  end
 
-  # test "should update car" do
-  #   patch car_url(@car), params: { car: { make: @car.make, model: @car.model, year: @car.year } }
-  #   assert_redirected_to car_url(@car)
-  # end
+  test "should update car" do
+    patch car_url(@car), params: { car: { make: @car.make, model: @car.model, year: @car.year } }
+    assert_redirected_to car_url(@car)
+  end
 
-  # test "should destroy car" do
-  #   assert_difference('Car.count', -1) do
-  #     delete car_url(@car)
-  #   end
+  test "should not update car with missing attribute values" do
+    patch car_url(@car), params: { car: { make: nil, model: nil, year: nil } }
+    assert_response :success
+  end
 
-  #   assert_redirected_to cars_url
-  # end
+  test "should destroy car" do
+    assert_difference('Car.count', -1) do
+      delete car_url(@car)
+    end
+    assert_redirected_to cars_url
+  end
 
 end
